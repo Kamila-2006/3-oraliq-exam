3a2c08aa30244da5d3dfe13dc5e8b798bb9505db
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-01-20 17:24:37 +0500

    modern blog

--- posts/views.py
+++ posts/views.py
@@ -13,17 +13,34 @@ def home(request):
     category_id = request.GET.get('category')
     author_id = request.GET.get('author')
     tag_id = request.GET.get('tag')
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+    sort_by = request.GET.get('sort', 'latest')
+    search = request.GET.get('query')
     if category_id:
         posts = posts.filter(category__id=category_id)
     if author_id:
         posts = posts.filter(author__id=author_id)
     if tag_id:
         posts = posts.filter(tags__id=tag_id)
+    if start_date and end_date:
+        posts = posts.filter(posted_at__range=(start_date, end_date))
+    if start_date:
+        posts = posts.filter(posted_at__gte=start_date)
+    if end_date:
+        posts = posts.filter(posted_at__lte=end_date)
+    if sort_by == 'latest':
+        posts = posts.order_by('-posted_at')
+    elif sort_by == 'oldest':
+        posts = posts.order_by('posted_at')
+    if search:
+        posts = posts.filter(title__icontains=search)
     ctx = {
         'posts':posts,
         'catalogs':catalogs,
         'authors':authors,
-        'tags':tags
+        'tags':tags,
+        'sort_by':sort_by
     }
     return render(request, 'index_with_side_bar.html', ctx)
 
